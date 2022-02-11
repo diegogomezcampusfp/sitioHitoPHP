83bbe1a723741c91a222f0baf8cd0362244508ee
Diego Gómez González <93382813+diegogomezcampusfp@users.noreply.github.com>
2022-02-11 09:17:05 +0100

    Update funcion.php

--- funcion.php
+++ funcion.php
@@ -5,6 +5,6 @@ $n=$_POST["n"];
     $nh=dechex($nb);
     $no=decoct($nb);
 
-    echo "<h3>El número ".$n." en decimal es: ".$nb."</h3>";
-    echo "<h3>El número ".$n." en hexadecinal es: ".$nh."</h3>";
-    echo "<h3>El número ".$n." en octal es: ".$no."</h3>";
+    echo "<h3>en decimal es: ".$nb."</h3>";
+    echo "<h3>en hexadecinal es: ".$nh."</h3>";
+    echo "<h3>en octal es: ".$no."</h3>";
