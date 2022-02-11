81311feb42431c88c82ee994491f1a35e9300b72
Diego Gómez González <93382813+diegogomezcampusfp@users.noreply.github.com>
2022-02-11 09:11:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,18 @@
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Sitio PHP</title>
-    <!-- CSS de bootstrap -->
-    <link href="css/estilo.css" rel="stylesheet">
-
-  </head>
-  <body>
-    <?php
-    echo "<h1> Hito Diego Gómez González </h1> ";
-    //bindec
-    echo "<h2> Cambio de binario a decimal </h2>";
-    echo "<p> Introduzco el número binario 001110001 usando BINDEC.</p>";
-    echo bindec('<h1>001110001</h1>');
-    //octdec
-    echo "<h2> Cambio de octal a decimal </h2>";
-    echo "<p> Introduzco el número 50 usando OCTDEC.</p>";
-    echo octdec('<h1>50</h1>');
-    //hexdec
-    echo "<h2> Cambio de hexadecimal a decimal </h2>";
-    echo "<p> Introduzco mi nombre usando HEXDEC.</p>";
-    echo hexdec('<h1>Diego</h1>');
-    //base_convert
-    echo "<h2> Muestra en una cadena el número introducido en base a_base, es decir: </h2>";
-    echo "<p> Valores entre 2 y 36, los digitos que tengan una base mayor que 10 serán representados con letras de la a a la z. </p>";
-    $hex = 'DIEGO';
-    echo base_convert($hex, 20, 2);
-?>
-  </body>
-  <!-- Plugins bootstrap -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conversión PHP</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+    <h2>Hito 1</h2>
+    <form action="calcular.php" method="post">
+            <label for="conversion">Convertir número</label>
+            <input type="number" id="conversion" name="n">
+        <button type="submit" class="btn btn-primary">Convertir</button>
+    </form>
+</body>
 </html>
